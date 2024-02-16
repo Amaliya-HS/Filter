f0de31a7558a44daf1780491602135d8f4b3bc8a
AmaliyaHS <77839112+AmaliyaHS@users.noreply.github.com>
2024-02-16 14:43:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,16 @@
 from dogFilter_app import VideoCameraSatu
 from mataMulut_app import VideoCameraDua
 from zoomIn_zoomOut_app import VideoCameraTiga
-from flask import Flask, render_template, Response, request
-from flask_cors import CORS
-import json
+from flask import Flask, render_template, Response
 
 app = Flask(__name__)
 
-camera_info = None
-
-@app.route('/camera-url', methods=['POST'])
-def get_camera_url():
-    data = request.json
-    ip_address = data['ip']
-    port = data['port']
-    camera_url = f"http://{ip_address}:{port}/video_feed1"
-    return camera_url
-
-def genSatu():
-    global camera_info
-    if camera_info:
-        camera = VideoCameraSatu(camera_info)
-        while True:
-            frame = camera.get_frame()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+def genSatu(dogFilter_app):
+    while True:
+        frame = dogFilter_app.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n'+frame
+               +b'\r\n\r\n')
 
 def genDua(mataMulut_app):
     while True:
@@ -42,7 +28,8 @@ def genTiga(zoomIn_zoomOut_app):
         
 @app.route('/video_feed1')
 def video_feed1():
-    return Response(genSatu(), mimetype='multipart/x-mixed-replace; boundary=frame')   
+    return Response(genSatu(VideoCameraSatu()),
+        mimetype='multipart/x-mixed-replace; boundary=frame')   
 
 @app.route('/fungsiDua')
 def video_feed2():
@@ -64,9 +51,7 @@ def index1():
 
 @app.route('/fungsiSatu.html')
 def index2():
-    camera_ip = request.remote_addr
-    camera_port = request.environ.get('REMOTE_PORT')
-    return render_template('fungsiSatu.html', camera_ip=camera_ip, camera_port=camera_port)
+    return render_template('fungsiSatu.html')
 
 @app.route('/fungsiDua.html')
 def openkamera2():
@@ -83,8 +68,3 @@ def latihan():
 @app.route('/team.html')
 def tim():
     return render_template('team.html')
-
-CORS(app)
-
-#if __name__ == '__main__':
-#    app.run(debug=True, host='0.0.0.0')
