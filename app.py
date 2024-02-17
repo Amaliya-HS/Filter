ec415abf3d0194c6c1b991b731761e46221da2cd
AmaliyaHS <77839112+AmaliyaHS@users.noreply.github.com>
2024-02-17 13:30:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from dogFilter_app import VideoCameraSatu
 from mataMulut_app import VideoCameraDua
 from zoomIn_zoomOut_app import VideoCameraTiga
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, jsonify, request
 
 app = Flask(__name__)
+
+camera_ip = ""
+camera_port = ""
+
+@app.route('/camera-info', methods=['GET'])
+def get_camera_info():
+    global camera_ip, camera_port
+    return jsonify({"ip": camera_ip, "port": camera_port})
 
 def genSatu(dogFilter_app):
     while True:
@@ -27,9 +35,10 @@ def genTiga(zoomIn_zoomOut_app):
                +b'\r\n\r\n')
         
 @app.route('/video_feed1')
-def video_feed1():
-    return Response(genSatu(VideoCameraSatu()),
-        mimetype='multipart/x-mixed-replace; boundary=frame')   
+def video_feed():
+    global camera_ip, camera_port
+    return Response(gen(VideoCamera(camera_ip, camera_port)),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/fungsiDua')
 def video_feed2():
