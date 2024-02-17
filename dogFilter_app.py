a68511fad80fc0b0736dc879d50b5c083674d56c
AmaliyaHS <77839112+AmaliyaHS@users.noreply.github.com>
2024-02-17 16:14:40 +0700

    Update dogFilter_app.py

--- dogFilter_app.py
+++ dogFilter_app.py
@@ -1,9 +1,7 @@
 import cv2
-import cv2
 import math
 import dlib
 from imutils import face_utils, rotate_bound
-from imutils.video import VideoStream
 
 def draw_sprite(frame, sprite, x_offset, y_offset):
     (h, w) = (sprite.shape[0], sprite.shape[1])
@@ -72,39 +70,44 @@ def get_face_boundbox(points, face_part):
     return (x, y, w, h)
 
 class VideoCameraSatu(object):
-    def __init__(self, camera_ip, camera_port):
-        self.video = cv2.VideoCapture(f"http://{camera_ip}:{camera_port}/video_feed1")
-
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+        
     def __del__(self):
         self.video.release()
         
     def get_frame(self):
-        image = self.video.read()
-    
+        ret, image = self.video.read()
+
         print("[INFO] loading facial landmark predictor...")
         model = "shape_predictor_68_face_landmarks.dat"
         predictor = dlib.shape_predictor(model)
-    
+
         detector = dlib.get_frontal_face_detector()
-                
+            
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         faces = detector(gray, 0)
-    
+
         for face in faces:
             (x, y, w, h) = (face.left(), face.top(), face.width(), face.height())
-    
+
             shape = predictor(gray, face)
             shape = face_utils.shape_to_np(shape)
-            incl = calculate_inclination(shape[17], shape[26])
-            is_mouth_open = (shape[66][1] - shape[62][1]) >= 10 
+            incl = calculate_inclination(
+                shape[17], shape[26]
+            )
+
+            is_mouth_open = (
+                shape[66][1] - shape[62][1]
+            ) >= 10 
             (x0, y0, w0, h0) = get_face_boundbox(shape, 6) 
-    
+
             (x3, y3, w3, h3) = get_face_boundbox(shape, 5)  
             apply_sprite(image, "static/gambar/dogs_nose.png", w3, x3, y3, incl, ontop=False)
             apply_sprite(image, "static/gambar/dog_ears.png", w, x, y, incl) 
-    
+
             if is_mouth_open:
                 apply_sprite(image, "static/gambar/dogs_tongue.png", w0, x0, y0, incl, ontop=False)
-    
+
         ret, jpeg = cv2.imencode('.jpg', image)
         return jpeg.tobytes()
