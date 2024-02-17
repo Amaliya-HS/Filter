72c64647fd113caeb160c2d0a475a88c8eeb9b18
AmaliyaHS <77839112+AmaliyaHS@users.noreply.github.com>
2024-02-17 22:51:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,76 +2,55 @@ from dogFilter_app import VideoCameraSatu
 from mataMulut_app import VideoCameraDua
 from zoomIn_zoomOut_app import VideoCameraTiga
 from flask import Flask, render_template, Response, request
-import socket
 
 app = Flask(__name__)
 
-def genSatu(dogFilter_app):
+def genFrame(video_camera):
     while True:
-        frame = dogFilter_app.get_frame()
+        frame = video_camera.get_frame()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n'+frame
                +b'\r\n\r\n')
 
-def genDua(mataMulut_app):
-    while True:
-        frame = mataMulut_app.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n'+frame
-               +b'\r\n\r\n')
-        
-def genTiga(zoomIn_zoomOut_app):
-    while True:
-        frame = zoomIn_zoomOut_app.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n'+frame
-               +b'\r\n\r\n')
-        
-@app.route('/fungsiSatu')
-def video_feed1():
+def get_ip_and_port():
     ip_address = "0.0.0.0"
     port = request.environ['SERVER_PORT']
-    return Response(genSatu(VideoCameraSatu(ip_address, port)),
-        mimetype='multipart/x-mixed-replace; boundary=frame')   
+    return ip_address, port
+
+@app.route('/fungsiSatu')
+def video_feed1():
+    ip_address, port = get_ip_and_port()
+    return Response(genFrame(VideoCameraSatu(ip_address, port)),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')   
 
 @app.route('/fungsiDua')
 def video_feed2():
-    ip_address = "0.0.0.0"
-    port = request.environ['SERVER_PORT']
-    return Response(genDua(VideoCameraDua(ip_address, port)),
-        mimetype='multipart/x-mixed-replace; boundary=frame')
+    ip_address, port = get_ip_and_port()
+    return Response(genFrame(VideoCameraDua(ip_address, port)),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
     
 @app.route('/fungsiTiga')
 def video_feed3():
-    ip_address = "0.0.0.0"
-    port = request.environ['SERVER_PORT']
-    return Response(genTiga(VideoCameraTiga(ip_address, port)),
-        mimetype='multipart/x-mixed-replace; boundary=frame')
+    ip_address, port = get_ip_and_port()
+    return Response(genFrame(VideoCameraTiga(ip_address, port)),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/index.html')
-def index1():
-    return render_template('index.html')
-
 @app.route('/fungsiSatu.html')
-def index2():
-    return render_template('fungsiSatu.html')
+def openkamera1():
+    ip_address, port = get_ip_and_port()
+    return render_template('fungsiSatu.html', ip=ip_address, port=port)
 
 @app.route('/fungsiDua.html')
 def openkamera2():
-    return render_template('fungsiDua.html')
+    ip_address, port = get_ip_and_port()
+    return render_template('fungsiDua.html', ip=ip_address, port=port)
 
 @app.route('/fungsiTiga.html')
 def openkamera3():
-    return render_template('fungsiTiga.html')
+    ip_address, port = get_ip_and_port()
+    return render_template('fungsiTiga.html', ip=ip_address, port=port)
 
-@app.route('/latihan.html')
-def latihan():
-    return render_template('latihan.html')
-
-@app.route('/team.html')
-def tim():
-    return render_template('team.html')
