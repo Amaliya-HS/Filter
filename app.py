1c6c440c56e16202eb76c9b78abdaca9aa775e99
AmaliyaHS <77839112+AmaliyaHS@users.noreply.github.com>
2024-02-17 19:32:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from dogFilter_app import VideoCameraSatu
 from mataMulut_app import VideoCameraDua
 from zoomIn_zoomOut_app import VideoCameraTiga
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, jsonify
+import socket
 
 app = Flask(__name__)
+
+def get_server_port():
+    # Mendapatkan port server Flask yang digunakan oleh Gunicorn
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('localhost', 0))
+    _, port = sock.getsockname()
+    sock.close()
+    return port
 
 def genSatu(dogFilter_app):
     while True:
@@ -28,7 +37,9 @@ def genTiga(zoomIn_zoomOut_app):
         
 @app.route('/fungsiSatu')
 def video_feed1():
-    return Response(genSatu(VideoCameraSatu()),
+    ip_address = socket.gethostbyname(socket.gethostname())  # Mendapatkan alamat IP server
+    port = get_server_port()  # Mendapatkan port server Flask
+    return Response(genSatu(VideoCameraSatu(ip_address, port)),
         mimetype='multipart/x-mixed-replace; boundary=frame')   
 
 @app.route('/fungsiDua')
