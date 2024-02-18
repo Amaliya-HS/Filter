489f08d638dfea44c94ab66ab907a4a96971346e
AmaliyaHS <77839112+AmaliyaHS@users.noreply.github.com>
2024-02-18 21:20:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,45 +1,16 @@
-from dogFilter_app import VideoCameraSatu
-from mataMulut_app import VideoCameraDua
-from zoomIn_zoomOut_app import VideoCameraTiga
-from flask import Flask, render_template, Response, request
+from dogFilter_app import get_frame
+from flask import Flask, render_template, Response, request, jsonify
 
 app = Flask(__name__)
 
-def genFrame(video_camera):
-    while True:
-        frame = video_camera.get_frame()
-        if frame is None:
-            continue
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n'+frame
-               +b'\r\n\r\n')
-
-@app.route('/send-frame', methods=['POST'])
-def receive_camera_url():
-    frame_data = request.json['frame']
-    img_data = frame_data.split(',')[1]
-    img_bytes = base64.b64decode(img_data)
-    with open('temp_frame.jpg', 'wb') as f:
-        f.write(img_bytes)
-    return f
-
-@app.route('/fungsiSatu')
-def video_feed1():
-    gunicorn_ip = receive_camera_url()
-    return Response(genFrame(VideoCameraSatu(gunicorn_ip)),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')   
-
-@app.route('/fungsiDua')
-def video_feed2():
-    gunicorn_ip = receive_camera_url()
-    return Response(genFrame(VideoCameraDua(gunicorn_ip)),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
-    
-@app.route('/fungsiTiga')
-def video_feed3():
-    gunicorn_ip = receive_camera_url()
-    return Response(genFrame(VideoCameraTiga(gunicorn_ip)),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+@app.route('/filter-one', methods=['POST'])
+def process_filter_one():
+    image_data = request.get_data()
+    image_array = np.frombuffer(image_data, dtype=np.uint8)
+    image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+    processed_image = get_frame(image)
+    processed_image_list = processed_image.tolist()
+    return jsonify({'processed_image': processed_image_list})
 
 @app.route('/')
 def index():
@@ -51,18 +22,15 @@ def index1():
 
 @app.route('/fungsiSatu.html')
 def openkamera1():
-    gunicorn_ip = request.host.split(':')[0]
-    return render_template('fungsiSatu.html', ip=gunicorn_ip)
+    return render_template('fungsiSatu.html')
 
 @app.route('/fungsiDua.html')
 def openkamera2():
-    gunicorn_ip = request.host.split(':')[0]
-    return render_template('fungsiDua.html', ip=gunicorn_ip)
+    return render_template('fungsiDua.html')
 
 @app.route('/fungsiTiga.html')
 def openkamera3():
-    gunicorn_ip = request.host.split(':')[0]
-    return render_template('fungsiTiga.html', ip=gunicorn_ip)
+    return render_template('fungsiTiga.html')
 
 @app.route('/latihan.html')
 def latihan():
