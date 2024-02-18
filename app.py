187d94b0948221918ca7be1ee5e67c57c0687fd0
AmaliyaHS <77839112+AmaliyaHS@users.noreply.github.com>
2024-02-18 18:20:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,26 +13,26 @@ def genFrame(video_camera):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-def get_address():
+def get_ip_and_port():
     ip = "0.0.0.0"
     port = request.environ['SERVER_PORT']
     return ip, port
 
 @app.route('/fungsiSatu')
 def video_feed1():
-    ip_address, port = get_address()
+    ip_address, port = get_ip_and_port()
     return Response(genFrame(VideoCameraSatu(ip_address, port)),
                     mimetype='multipart/x-mixed-replace; boundary=frame')   
 
 @app.route('/fungsiDua')
 def video_feed2():
-    ip_address, port = get_address()
+    ip_address, port = get_ip_and_port()
     return Response(genFrame(VideoCameraDua(ip_address, port)),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
     
 @app.route('/fungsiTiga')
 def video_feed3():
-    ip_address, port = get_address()
+    ip_address, port = get_ip_and_port()
     return Response(genFrame(VideoCameraTiga(ip_address, port)),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
