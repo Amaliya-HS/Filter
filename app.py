b2872187309465e81883af228555c381e83b46bf
AmaliyaHS <77839112+AmaliyaHS@users.noreply.github.com>
2024-02-19 20:01:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request
 from dogFilter_app import VideoCameraSatu
 from mataMulut_app import VideoCameraDua
 from zoomIn_zoomOut_app import VideoCameraTiga
@@ -13,7 +13,8 @@ def gen(camera):
 
 @app.route('/fungsiSatu')
 def video_feed1():
-    return Response(gen(VideoCameraSatu()), mimetype='multipart/x-mixed-replace; boundary=frame')
+    gunicorn_ip = request.host.split(':')[0]
+    return Response(gen(VideoCameraSatu("https://{gunicorn_ip}/fungsiSatu")), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/fungsiDua')
 def video_feed2():
