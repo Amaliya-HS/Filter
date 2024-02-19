d3bf7df48466fc530e43e7a5e84bef91fe0ab9d5
AmaliyaHS <77839112+AmaliyaHS@users.noreply.github.com>
2024-02-19 19:01:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,45 +1,40 @@
-from dogFilter_app import process_image
-from flask import Flask, render_template, Response, request, jsonify
-import cv2
-import base64
+from flask import Flask, render_template, Response
+from dogFilter_app import VideoCameraSatu
+from mataMulut_app import VideoCameraDua
+from zoomIn_zoomOut_app import VideoCameraTiga
 
 app = Flask(__name__)
 
-@app.route('/process-image', methods=['POST'])
-def process_image_route():
-    # Terima data gambar dari klien
-    image_data = request.data.decode('utf-8')
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-    # Proses gambar
-    processed_image = process_image(image_data)
+@app.route('/fungsiSatu')
+def video_feed1():
+    return Response(gen(VideoCameraSatu()), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-    # Kirim gambar yang diproses sebagai respons JSON
-    return jsonify({'processed_image': processed_image})
+@app.route('/fungsiDua')
+def video_feed2():
+    return Response(gen(VideoCameraDua()), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/fungsiTiga')
+def video_feed3():
+    return Response(gen(VideoCameraTiga()), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/index.html')
-def index1():
-    return render_template('index.html')
-
 @app.route('/fungsiSatu.html')
-def openkamera1():
+def fungsiSatu():
     return render_template('fungsiSatu.html')
 
 @app.route('/fungsiDua.html')
-def openkamera2():
+def fungsiDua():
     return render_template('fungsiDua.html')
 
 @app.route('/fungsiTiga.html')
-def openkamera3():
+def fungsiTiga():
     return render_template('fungsiTiga.html')
-
-@app.route('/latihan.html')
-def latihan():
-    return render_template('latihan.html')
-
-@app.route('/team.html')
-def tim():
-    return render_template('team.html')
